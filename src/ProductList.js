f28cefde799d55205e70c7e149250652c94fcd04
Pankaj <pankaj200635@gmail.com>
2021-06-28 11:53:38 +0530

    Delete Product API integration

--- src/ProductList.js
+++ src/ProductList.js
@@ -1,52 +1,65 @@
 import Header from './header';
 
-import React,{useState,useEffect} from 'react';
-import {Table} from 'react-bootstrap'
+import React, { useState, useEffect } from 'react';
+import { Table } from 'react-bootstrap'
 
 function ProducList() {
 
-    const [data,setData]=useState([]);
-    useEffect(async ()=>{
+  const [data, setData] = useState([]);
+  useEffect(() => {
+    getData();
+  }, []);
 
-    let result= await fetch("http://localhost:8000/api/list");
-
-    result=await result.json();
+  async function deleteOperation(id) {
+    let result = await fetch("http://localhost:8000/api/delete/" + id, {
+      method: "DELETE"
+    });
+    result = await result.json();
+    getData();
+    alert(id);
+  }
+  async function getData() {
+    let result = await fetch("http://localhost:8000/api/list");
+    result = await result.json();
     setData(result);
-    console.log("data",data);
+    console.log("data", data);
+  }
 
-    },[]);
-    return (
-        <div>
-            <Header />
-            <h1>Product List</h1>
-            <div className="col-sm-10 offset-sm-1">
-            <Table >
-  <thead>
-    <tr>
-      <th>#</th>
-      <th>Product Name</th>
-      <th>Product Image</th>
-      <th>Price</th>
-      <th>Desccription</th>
 
-    </tr>
-  </thead>
-  <tbody>
-{
-      data.map((item)=>
-    <tr>
-      <td>{item.id}</td>
-      <td>{item.name}</td>
-      <td><img style={{width:140, resizeMode: 'cover', justifyContent: 'center', alignItems: 'center'}} src={"http://localhost:8000/"+item.file_path}/></td>
-      <td>{item.price}</td>
-      <td>{item.description}</td>
-    </tr>)}
-  </tbody>
-</Table>
-</div>
+  return (
+    <div>
+      <Header />
+      <h1>Product List</h1>
+      <div className="col-sm-10 offset-sm-1">
+        <Table >
+          <thead>
+            <tr>
+              <th>#</th>
+              <th>Product Name</th>
+              <th>Product Image</th>
+              <th>Price</th>
+              <th>Desccription</th>
 
-        </div>
-    )
+            </tr>
+          </thead>
+          <tbody>
+            {
+              data.map((item) =>
+                <tr>
+                  <td>{item.id}</td>
+                  <td>{item.name}</td>
+                  <td><img style={{ width: 140, resizeMode: 'cover', justifyContent: 'center', alignItems: 'center' }} src={"http://localhost:8000/" + item.file_path} /></td>
+                  <td>{item.price}</td>
+                  <td>{item.description}</td>
+                  <td><button className="btn btn-success" onClick={() => { deleteOperation(item.id) }}>Delete</button></td>
+
+                </tr>)}
+          </tbody>
+        </Table>
+      </div>
+
+    </div>
+  )
 
 }
 
